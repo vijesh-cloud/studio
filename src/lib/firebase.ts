9e638574cd0bff3cd0b626fce59b1ec0300c7cf3
vijesh S <vijesh591010@gmail.com>
2025-08-08 06:08:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBHoKo3DfI5S-Jq8I0ePOkXtmOg2oOgyhk",
   authDomain: "ecoverse-dzcey.firebaseapp.com",
   projectId: "ecoverse-dzcey",
-  storageBucket: "ecoverse-dzcey.firebasestorage.app",
+  storageBucket: "ecoverse-dzcey.appspot.com",
   messagingSenderId: "352417571073",
   appId: "1:352417571073:web:a477bef8d5a7f1ec543b00"
 };
