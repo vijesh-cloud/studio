9031f6e077fb4e44f6e0e78aa714cbba7b3c804f
vijesh S <vijesh591010@gmail.com>
2025-08-08 06:17:10 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,17 +3,18 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHoKo3DfI5S-Jq8I0ePOkXtmOg2oOgyhk",
-  // authDomain is now set dynamically below
-  projectId: "ecoverse-dzcey",
-  storageBucket: "ecoverse-dzcey.appspot.com",
-  messagingSenderId: "352417571073",
-  appId: "1:352417571073:web:a477bef8d5a7f1ec543b00"
+  "projectId": "ecoverse-dzcey",
+  "appId": "1:352417571073:web:a477bef8d5a7f1ec543b00",
+  "storageBucket": "ecoverse-dzcey.appspot.com",
+  "apiKey": "AIzaSyBHoKo3DfI5S-Jq8I0ePOkXtmOg2oOgyhk",
+  "authDomain": "ecoverse-dzcey.firebaseapp.com",
+  "messagingSenderId": "352417571073"
 };
+
 
 // Dynamically set authDomain for browser environments to avoid auth/unauthorized-domain errors
 // in development environments with dynamic URLs.
-if (typeof window !== 'undefined') {
+if (typeof window !== 'undefined' && window.location.hostname !== 'localhost') {
   (firebaseConfig as any).authDomain = window.location.hostname;
 }
 
