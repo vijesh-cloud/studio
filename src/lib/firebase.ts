faad16006910310c321c9bbd29f3ce96bc7b4711
vijesh S <vijesh591010@gmail.com>
2025-08-08 04:00:33 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBHoKo3DfI5S-Jq8I0ePOkXtmOg2oOgyhk",
+  authDomain: "ecoverse-dzcey.firebaseapp.com",
+  projectId: "ecoverse-dzcey",
+  storageBucket: "ecoverse-dzcey.firebasestorage.app",
+  messagingSenderId: "352417571073",
+  appId: "1:352417571073:web:a477bef8d5a7f1ec543b00"
 };
 
 // Initialize Firebase
