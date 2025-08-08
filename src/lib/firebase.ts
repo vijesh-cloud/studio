d1710d55f1b174f50c1349ff96dcf4697a8ec845
vijesh S <vijesh591010@gmail.com>
2025-08-08 06:28:13 +0000

    FirebaseError: Firebase: Error (auth/unauthorized-domain).     at create

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,16 +7,9 @@ const firebaseConfig = {
   "appId": "1:352417571073:web:a477bef8d5a7f1ec543b00",
   "storageBucket": "ecoverse-dzcey.appspot.com",
   "apiKey": "AIzaSyBHoKo3DfI5S-Jq8I0ePOkXtmOg2oOgyhk",
-  "authDomain": "ecoverse-dzcey.firebaseapp.com",
+  "authDomain": "localhost",
   "messagingSenderId": "352417571073"
 };
-
-
-// Dynamically set authDomain for browser environments to avoid auth/unauthorized-domain errors
-// in development environments with dynamic URLs.
-if (typeof window !== 'undefined' && window.location.hostname !== 'localhost') {
-  (firebaseConfig as any).authDomain = window.location.hostname;
-}
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
